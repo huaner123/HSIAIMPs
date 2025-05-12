b2c1ec20a51a5269bb7370bbb0f9f963d750514b
Huan Chen <chkalate@gmail.com>
2025-05-11 22:34:25 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+from mp_hsi import *
+
+"""
+    This is for testing the MP_HSI base module. 
+    Be careful running all lines below because it may take days to complete.
+"""
+
+spectral_plots_pure_samples()
+spectral_plots_combined_dataset()
+tsne_umap_plots()
+classification_table1()  # this may take an hour to finish all hyperparameter optimizations
+classification_table2()  # this may take days (e.g., 4 days) to finish all hyperparameter optimizations
